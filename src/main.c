5258f62391d3f1ec70c1dd09a9213eddf5d59da3
betbatesc <betbatesc@gmail.com>
2018-01-08 04:38:14 -0800

    IRQs and PIT support

--- src/main.c
+++ src/main.c
@@ -1,15 +1,24 @@
 #include "monitor.h"
 #include "descriptor_tables.h"
+#include "timer.h"
 
 int main(struct multiboot *mboot_ptr) {
+
 	// All out initialisation calls will go in here.
 	// Initialise all the ISRs and segmentation
 	init_descriptor_tables();   
+	
 	// Initialise the screen (by clearing it)
 	monitor_clear();
+
 	// Write out a sample string
 	monitor_write("ChetOS - A simple x86 Operating System\n");
+
 	asm volatile("int $0x3");
 	asm volatile("int $0x4");
+
+	asm volatile("sti");
+	init_timer(50);
+
 	return 0;
 }

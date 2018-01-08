a51e612df75d844af0eaed5b17ba7e8abf2d20cc
betbatesc <betbatesc@gmail.com>
2018-01-08 01:33:09 -0800

    Adding GDT and IDT support

--- src/main.c
+++ src/main.c
@@ -1,11 +1,15 @@
 #include "monitor.h"
+#include "descriptor_tables.h"
 
 int main(struct multiboot *mboot_ptr) {
 	// All out initialisation calls will go in here.
-	// return 0xDEADBABA;   
+	// Initialise all the ISRs and segmentation
+	init_descriptor_tables();   
 	// Initialise the screen (by clearing it)
 	monitor_clear();
 	// Write out a sample string
 	monitor_write("ChetOS - A simple x86 Operating System\n");
+	asm volatile("int $0x3");
+	asm volatile("int $0x4");
 	return 0;
 }

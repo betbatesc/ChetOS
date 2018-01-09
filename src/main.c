11063bd1e05af50a62210ff6e04058afbd1c102d
betbatesc <betbatesc@gmail.com>
2018-01-09 15:17:37 -0800

    Paging support

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "monitor.h"
 #include "descriptor_tables.h"
 #include "timer.h"
+#include "paging.h"
 
 int main(struct multiboot *mboot_ptr) {
 
@@ -11,14 +12,19 @@ int main(struct multiboot *mboot_ptr) {
 	// Initialise the screen (by clearing it)
 	monitor_clear();
 
+	initialise_paging();
+
 	// Write out a sample string
 	monitor_write("ChetOS - A simple x86 Operating System\n");
 
 	asm volatile("int $0x3");
 	asm volatile("int $0x4");
 
-	asm volatile("sti");
-	init_timer(50);
+	//asm volatile("sti");
+	//init_timer(50);
+	
+	u32int *ptr = (u32int *)0xA0000000;
+	u32int do_page_fault = *ptr;
 
 	return 0;
 }

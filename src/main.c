89410884efb0bd77facc08fb6c44b676b005615d
betbatesc <betbatesc@gmail.com>
2018-01-15 22:35:19 -0800

    User mode support

--- src/main.c
+++ src/main.c
@@ -6,6 +6,7 @@
 #include "fs.h"
 #include "initrd.h"
 #include "task.h"
+#include "syscall.h"
 
 #define CHECK_FLAG(flags,bit)	((flags)&(1 << (bit)))
 
@@ -25,47 +26,55 @@ int main(struct multiboot *mboot_ptr, u32int initial_stack) {
 	// Write out a sample string
 	monitor_write("ChetOS - A simple x86 Operating System\n");
 
+	// Initialise the PIT to 100Hz
 	asm volatile("sti");
 	init_timer(50);
 
 	// Find the location of our initial ramdisk.
-    ASSERT(mboot_ptr->mods_count > 0);
-    u32int initrd_location = *((u32int*)mboot_ptr->mods_addr);
-    u32int initrd_end = *(u32int*)(mboot_ptr->mods_addr+4);
-    // Don't trample our module with placement accesses, please!
-    placement_address = initrd_end;
+   	ASSERT(mboot_ptr->mods_count > 0);
+    	u32int initrd_location = *((u32int*)mboot_ptr->mods_addr);
+    	u32int initrd_end = *(u32int*)(mboot_ptr->mods_addr+4);
+    	// Don't trample our module with placement accesses, please!
+    	placement_address = initrd_end;
 
-    // Start paging.
-    initialise_paging();
+    	// Start paging.
+    	initialise_paging();
 
-    // Start multitasking.
-    initialise_tasking();
+    	// Start multitasking.
+    	initialise_tasking();
 
-    // Initialise the initial ramdisk, and set it as the filesystem root.
-    fs_root = initialise_initrd(initrd_location);
+    	// Initialise the initial ramdisk, and set it as the filesystem root.
+    	fs_root = initialise_initrd(initrd_location);
 
-    // Create a new process in a new address space which is a clone of this.
-    int ret = fork();
+	initialise_syscalls();
 
-    monitor_write("fork() returned ");
-    monitor_write_hex(ret);
-    monitor_write(", and getpid() returned ");
-    monitor_write_hex(getpid());
-    monitor_write("\n============================================================================\n");
+	switch_to_user_mode();
 
-    // The next section of code is not reentrant so make sure we aren't interrupted during.
-    asm volatile("cli");
-    // list the contents of /
-    int i = 0;
-    struct dirent *node = 0;
-    while ( (node = readdir_fs(fs_root, i)) != 0)
-    {
-        monitor_write("Found file ");
-        monitor_write(node->name);
-        fs_node_t *fsnode = finddir_fs(fs_root, node->name);
+	syscall_monitor_write("Hello, user world!\n");
 
-        if ((fsnode->flags&0x7) == FS_DIRECTORY)
-        {
+	/*
+    	// Create a new process in a new address space which is a clone of this.
+    	int ret = fork();
+
+    	monitor_write("fork() returned ");
+    	monitor_write_hex(ret);
+    	monitor_write(", and getpid() returned ");
+    	monitor_write_hex(getpid());
+    	monitor_write("\n============================================================================\n");
+
+    	// The next section of code is not reentrant so make sure we aren't interrupted during.
+    	asm volatile("cli");
+    	// list the contents of /
+    	int i = 0;
+    	struct dirent *node = 0;
+    	while ( (node = readdir_fs(fs_root, i)) != 0)
+    	{
+        	monitor_write("Found file ");
+        	monitor_write(node->name);
+        	fs_node_t *fsnode = finddir_fs(fs_root, node->name);
+
+        	if ((fsnode->flags&0x7) == FS_DIRECTORY)
+        	{
             monitor_write("\n\t(directory)\n");
         }
         else
@@ -84,7 +93,7 @@ int main(struct multiboot *mboot_ptr, u32int initial_stack) {
     monitor_write("\n");
 
     asm volatile("sti");
-	
+	*/
 	/*
 	monitor_write("\nFlags: ");
 	monitor_write_hex(mboot_ptr->flags);
